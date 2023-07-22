4e02dfaeebac5055941235b33deb193242d1dd2c
OlegOTB <olegotb@mail.ru>
2023-07-22 17:22:57 +0300

    Lesson 11

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
   const formatCount = () => {
     return count === 0 ? "empty" : count;
   };
@@ -16,8 +17,26 @@ const Counter = () => {
   const handelDecrement = () => {
     setCount((prevState) => prevState - 1);
   };
+  const handelTagChange = (id) => {
+    setTags((prevState) => prevState.filter((tag) => tag !== id));
+  };
+  const renderTags = () => {
+    return tags.map((tag) => (
+      <li
+        key={tag}
+        className="btn btn-primary btm-sm m2"
+        onClick={() => handelTagChange(tag)}
+      >
+        {tag}
+      </li>
+    ));
+  };
+  if (tags.length !== 0) {
+    return <ul>{renderTags()}</ul>;
+  }
   return (
     <>
+      <ul>{renderTags()}</ul>
       <span className={getBageClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
